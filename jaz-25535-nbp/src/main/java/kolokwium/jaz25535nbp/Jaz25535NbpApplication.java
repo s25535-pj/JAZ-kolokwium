fdad346bd3e0e3230d77dc165a4d2d714ee1d48f
s25535-pj <s25535@pjwstk.edu.pl>
2023-06-24 12:45:23 +0200

    Wydmuszka

---
+++ jaz-25535-nbp/src/main/java/kolokwium/jaz25535nbp/Jaz25535NbpApplication.java
@@ -0,0 +1,13 @@
+package kolokwium.jaz25535nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz25535NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaz25535NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-25535-nbp/src/test/java/kolokwium/jaz25535nbp/Jaz25535NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package kolokwium.jaz25535nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz25535NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
